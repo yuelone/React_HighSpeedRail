5576ee7d57d52ecd71602af6774a5b686f23532a
yuelone <yuelone@chenyuelongdeMacBook-Pro.local>
2023-01-22 23:38:18 +0800

    setting(project):project construct

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import image from "./==.gif";
+
+function App() {
+  return (
+    <div>
+      start
+      {/* <img src={image} /> */}
+      {console.log("123", 123)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux'
+
+// import configureStore from 'Redux/store'
+
+// import 'core-js'
+
+// const store = configureStore()
+
+import App from './app';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    {/* <Provider store={store}> */}
+      {/* <App store={store} /> */}
+      <App />
+    {/* </Provider> */}
+    ,
+  </>,
+);
